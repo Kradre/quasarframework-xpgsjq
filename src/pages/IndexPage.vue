ef896f7a9a6b38366619354cbc845e93858acae0
Kradre <kradre@pngamers.org>
2022-09-21 20:55:10 +0700

    2 files modified

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,6 +1,6 @@
 <template class="bg-black">
   <div class="full-width text-center bg-tmp row justify-center items-center">
-    <h2 class="text-white q-mt-none">1BID = Your bridge to the world assets</h2>
+    <h3 class="text-white q-mt-none">1BID = Your bridge to the world assets</h3>
   </div>
 </template>
 
